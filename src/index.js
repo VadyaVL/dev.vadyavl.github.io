f7db1413747f226269cf7b18232b4090de50b511
Vadym <vadyavl@gmail.com>
2018-04-22 13:39:38 +0300

    some clean

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MainPage from './pages/main-page';
+
+import './index.css';
+
+ReactDOM.render(<MainPage />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/pages/main-page.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './main-page.css';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="main-page-container">
+        <header className="main-page-header">
+          <h1 className="main-page-title">Welcome to React</h1>
+        </header>
+        <p className="main-page-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
