34c97a201586f70355c9b128634978020646659f
Vadym <vadyavl@gmail.com>
2018-05-05 00:27:33 +0300

    not use base64 for image

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -50,9 +50,15 @@ module.exports = {
             use: ["style-loader", "css-loader", "sass-loader"]
         },
         {
-          test: /\.jpg$/,
-          loader: "url-loader?mimetype=image/jpg" 
-       }
+          test: /\.(png|jp(e*)g|svg)$/,  
+          use: [{
+            loader: 'url-loader',
+            options: { 
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]'
+            } 
+          }]
+        }
       ]
     },
     plugins: [htmlPlugin, uglifyJsPlugin, minifyPlugin/*, compressionPlugin*/]
